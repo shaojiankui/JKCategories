d0f20708f19e9154c2d5c2fb9d938243332952b3
Jakey <jakey@Jakey-Pro.local>
2014-12-20 15:02:23 +0800

    NSObject+Property

---
+++ categories/Foundation/NSObject/NSObject+Property.m
@@ -0,0 +1,31 @@
+//
+//  NSObject+Property.m
+//  categories
+//
+//  Created by Jakey on 14/12/20.
+//  Copyright (c) 2014年 www.skyfox.org. All rights reserved.
+//
+
+#import "NSObject+Property.h"
+#import <Foundation/Foundation.h>
+#import <objc/runtime.h>
+
+@implementation NSObject (Property)
+-(NSDictionary *)propertyDictionary
+{
+    //创建可变字典
+    NSMutableDictionary *dict = [NSMutableDictionary dictionary];
+    unsigned int outCount;
+    objc_property_t *props = class_copyPropertyList([self class], &outCount);
+    for(int i=0;i<outCount;i++){
+        objc_property_t prop = props[i];
+        NSString *propName = [[NSString alloc]initWithCString:property_getName(prop) encoding:NSUTF8StringEncoding];
+        id propValue = [self valueForKey:propName];
+        if(propValue){
+            [dict setObject:propValue forKey:propName];
+        }
+    }
+    free(props);
+    return dict;
+}
+@end
