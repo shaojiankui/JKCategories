ae39bfa54b321d04f4d8271fc1233d4617cf9fb5
Jakey <i@skyfox.org>
2014-12-22 08:41:17 +0800

    UIWebView+HTML5

---
+++ categories/UIKit/UIColor/UIColor+Web.h
@@ -0,0 +1,16 @@
+//
+//  UIColor+Web.h
+//  categories
+//
+//  Created by Jakey on 14/12/22.
+//  Copyright (c) 2014年 www.skyfox.org. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIColor (Web)
+// 获取canvas用的颜色字符串
+- (NSString *)canvasColorString;
+//获取网页颜色字串
+- (NSString *)webColorString;
+@end
