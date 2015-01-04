5720b6c7420fe3fd842b23bcf92499bdd6b0b842
Jakey <i@skyfox.org>
2015-01-04 12:05:17 +0800

    UIImage+Orientation

---
+++ categories/UIKit/UIImage/UIImage+Orientation.h
@@ -0,0 +1,19 @@
+//
+//  UIImage+Orientation.h
+//  categories
+//
+//  Created by Jakey on 15/1/4.
+//  Copyright (c) 2015年 www.skyfox.org. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+CGFloat DegreesToRadians(CGFloat degrees) {return degrees * M_PI / 180;};
+CGFloat RadiansToDegrees(CGFloat radians) {return radians * 180/M_PI;};
+@interface UIImage (Orientation)
++ (UIImage *)fixOrientation:(UIImage *)srcImg;
+/*垂直翻转*/
+- (UIImage *)flipVertical;
+/*水平翻转*/
+- (UIImage *)flipHorizontal;
+
+@end
---
+++ categories/UIKit/UIImage/UIImage+Orientation.m
@@ -0,0 +1,102 @@
+//
+//  UIImage+Orientation.m
+//  categories
+//
+//  Created by Jakey on 15/1/4.
+//  Copyright (c) 2015年 www.skyfox.org. All rights reserved.
+//
+
+#import "UIImage+Orientation.h"
+
+@implementation UIImage (Orientation)
++ (UIImage *)fixOrientation:(UIImage *)srcImg {
+    if (srcImg.imageOrientation == UIImageOrientationUp) return srcImg;
+    CGAffineTransform transform = CGAffineTransformIdentity;
+    switch (srcImg.imageOrientation) {
+        case UIImageOrientationDown:
+        case UIImageOrientationDownMirrored:
+            transform = CGAffineTransformTranslate(transform, srcImg.size.width, srcImg.size.height);
+            transform = CGAffineTransformRotate(transform, M_PI);
+            break;
+        case UIImageOrientationLeft:
+        case UIImageOrientationLeftMirrored:
+            transform = CGAffineTransformTranslate(transform, srcImg.size.width, 0);
+            transform = CGAffineTransformRotate(transform, M_PI_2);
+            break;
+        case UIImageOrientationRight:
+        case UIImageOrientationRightMirrored:
+            transform = CGAffineTransformTranslate(transform, 0, srcImg.size.height);
+            transform = CGAffineTransformRotate(transform, -M_PI_2);
+            break;
+        case UIImageOrientationUp:
+        case UIImageOrientationUpMirrored:
+            break;
+    }
+    switch (srcImg.imageOrientation) {
+        case UIImageOrientationUpMirrored:
+        case UIImageOrientationDownMirrored:
+            transform = CGAffineTransformTranslate(transform, srcImg.size.width, 0);
+            transform = CGAffineTransformScale(transform, -1, 1);
+            break;
+        case UIImageOrientationLeftMirrored:
+        case UIImageOrientationRightMirrored:
+            transform = CGAffineTransformTranslate(transform, srcImg.size.height, 0);
+            transform = CGAffineTransformScale(transform, -1, 1);
+            break;
+        case UIImageOrientationUp:
+        case UIImageOrientationDown:
+        case UIImageOrientationLeft:
+        case UIImageOrientationRight:
+            break;
+    }
+    CGContextRef ctx = CGBitmapContextCreate(NULL, srcImg.size.width, srcImg.size.height,
+                                             CGImageGetBitsPerComponent(srcImg.CGImage), 0,
+                                             CGImageGetColorSpace(srcImg.CGImage),
+                                             CGImageGetBitmapInfo(srcImg.CGImage));
+    CGContextConcatCTM(ctx, transform);
+    switch (srcImg.imageOrientation) {
+        case UIImageOrientationLeft:
+        case UIImageOrientationLeftMirrored:
+        case UIImageOrientationRight:
+        case UIImageOrientationRightMirrored:
+            CGContextDrawImage(ctx, CGRectMake(0,0,srcImg.size.height,srcImg.size.width), srcImg.CGImage);
+            break;
+        default:
+            CGContextDrawImage(ctx, CGRectMake(0,0,srcImg.size.width,srcImg.size.height), srcImg.CGImage);
+            break;
+    }
+    CGImageRef cgimg = CGBitmapContextCreateImage(ctx);
+    UIImage *img = [UIImage imageWithCGImage:cgimg];
+    CGContextRelease(ctx);
+    CGImageRelease(cgimg);
+    return img;
+}
+
+- (UIImage *)flip:(BOOL)isHorizontal {
+    CGRect rect = CGRectMake(0, 0, self.size.width, self.size.height);
+    if (UIGraphicsBeginImageContextWithOptions != NULL) {
+        UIGraphicsBeginImageContextWithOptions(rect.size, NO, 0);
+    } else {
+        UIGraphicsBeginImageContext(rect.size);
+    }
+    CGContextRef ctx = UIGraphicsGetCurrentContext();
+    CGContextClipToRect(ctx, rect);
+    if (isHorizontal) {
+        CGContextRotateCTM(ctx, M_PI);
+        CGContextTranslateCTM(ctx, -rect.size.width, -rect.size.height);
+    }
+    CGContextDrawImage(ctx, rect, self.CGImage);
+    UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    return image;
+}
+
+- (UIImage *)flipVertical {
+    return [self flip:NO];
+}
+
+- (UIImage *)flipHorizontal {
+    return [self flip:YES];
+}
+
+@end
