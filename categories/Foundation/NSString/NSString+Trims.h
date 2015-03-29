3f11425b17f432afa4632a5d343a6712622aaaf3
Jakey <jakey@Jakey-Pro.local>
2015-03-29 18:09:36 +0800

    NSString+Emoji NSString+Trims

---
+++ categories/Foundation/NSString/NSString+Trims.h
@@ -0,0 +1,17 @@
+//
+//  NSString+Trims.h
+//  IOS-Categories
+//
+//  Created by Jakey on 15/3/29.
+//  Copyright (c) 2015年 www.skyfox.org. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (Trims)
+- (NSString *)stringByStrippingHTML;
+- (NSString *)stringByRemovingScriptsAndStrippingHTML;
+
+- (NSString *)trimmingWhitespace;
+- (NSString *)trimmingWhitespaceAndNewlines;
+@end
---
+++ categories/Foundation/NSString/NSString+Trims.m
@@ -0,0 +1,37 @@
+//
+//  NSString+Trims.m
+//  IOS-Categories
+//
+//  Created by Jakey on 15/3/29.
+//  Copyright (c) 2015年 www.skyfox.org. All rights reserved.
+//
+
+#import "NSString+Trims.h"
+
+@implementation NSString (Trims)
+- (NSString *)stringByStrippingHTML {
+    return [self stringByReplacingOccurrencesOfString:@"<[^>]+>" withString:@"" options:NSRegularExpressionSearch range:NSMakeRange(0, self.length)];
+}
+
+
+- (NSString *)stringByRemovingScriptsAndStrippingHTML {
+    NSMutableString *mString = [self mutableCopy];
+    NSError *error;
+    NSRegularExpression *regex = [NSRegularExpression regularExpressionWithPattern:@"<script[^>]*>[\\w\\W]*</script>" options:NSRegularExpressionCaseInsensitive error:&error];
+    NSArray *matches = [regex matchesInString:mString options:NSMatchingReportProgress range:NSMakeRange(0, [mString length])];
+    for (NSTextCheckingResult *match in [matches reverseObjectEnumerator]) {
+        [mString replaceCharactersInRange:match.range withString:@""];
+    }
+    return [mString stringByStrippingHTML];
+}
+
+
+- (NSString *)trimmingWhitespace
+{
+    return [self stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceCharacterSet]];
+}
+- (NSString *)trimmingWhitespaceAndNewlines
+{
+    return [self stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
+}
+@end
