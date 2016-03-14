0e3ca3cf9ca50c60b8bed6b68f88b2cde2e41e0a
Jakey <i@skyfox.org>
2016-03-14 10:22:50 +0800

    UICollectionView+ARDynamicCacheHeightLayoutCell

---
+++ Demos/UIKit/UICollectionView/UICollectionViewDemoViewController.m
@@ -0,0 +1,37 @@
+//
+//  UICollectionViewDemoViewController.m
+//  IOS-Categories
+//
+//  Created by Jakey on 16/3/14.
+//  Copyright © 2016年 www.skyfox.org. All rights reserved.
+//
+
+#import "UICollectionViewDemoViewController.h"
+
+@interface UICollectionViewDemoViewController ()
+
+@end
+
+@implementation UICollectionViewDemoViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
