8de6bcb1644a6d8a011d9de654c8fbbfb86cce44
Qilin Hu <andywhm@163.com>
2020-07-28 14:39:20 +0800

    使用 CGRect 函数更新 UIView+JKFrame 文件。

--- JKCategories/UIKit/UIView/UIView+JKFrame.m
+++ JKCategories/UIKit/UIView/UIView+JKFrame.m
@@ -9,79 +9,6 @@
 #import "UIView+JKFrame.h"
 
 @implementation UIView (JKFrame)
-#pragma mark - Shortcuts for the coords
-
-- (CGFloat)jk_top
-{
-    return self.frame.origin.y;
-}
-
-- (void)setJk_top:(CGFloat)y
-{
-    CGRect frame = self.frame;
-    frame.origin.y = y;
-    self.frame = frame;
-}
-
-- (CGFloat)jk_right
-{
-    return self.frame.origin.x + self.frame.size.width;
-}
-
-- (void)setJk_right:(CGFloat)right
-{
-    CGRect frame = self.frame;
-    frame.origin.x = right - self.frame.size.width;
-    self.frame = frame;
-}
-
-- (CGFloat)jk_bottom
-{
-    return self.frame.origin.y + self.frame.size.height;
-}
-
-- (void)setJk_bottom:(CGFloat)bottom
-{
-    CGRect frame = self.frame;
-    frame.origin.y = bottom - self.frame.size.height;
-    self.frame = frame;
-}
-
-- (CGFloat)jk_left
-{
-    return self.frame.origin.x;
-}
-
-- (void)setJk_left:(CGFloat)x
-{
-    CGRect frame = self.frame;
-    frame.origin.x = x;
-    self.frame = frame;
-}
-
-- (CGFloat)jk_width
-{
-    return self.frame.size.width;
-}
-
-- (void)setJk_width:(CGFloat)width
-{
-    CGRect frame = self.frame;
-    frame.size.width = width;
-    self.frame = frame;
-}
-
-- (CGFloat)jk_height
-{
-    return self.frame.size.height;
-}
-
-- (void)setJk_height:(CGFloat)height
-{
-    CGRect frame = self.frame;
-    frame.size.height = height;
-    self.frame = frame;
-}
 
 #pragma mark - Shortcuts for frame properties
 
@@ -104,6 +31,7 @@
     frame.size = size;
     self.frame = frame;
 }
+
 #pragma mark - Shortcuts for positions
 
 - (CGFloat)jk_centerX {
@@ -120,6 +48,71 @@
 
 - (void)setJk_centerY:(CGFloat)centerY {
     self.center = CGPointMake(self.center.x, centerY);
+}
+
+#pragma mark - Shortcuts for the coords
+
+- (CGFloat)jk_top {
+    return CGRectGetMinY(self.frame);
+}
+
+- (void)setJk_top:(CGFloat)y {
+    CGRect frame = self.frame;
+    frame.origin.y = y;
+    self.frame = frame;
+}
+
+- (CGFloat)jk_bottom {
+    CGRect frame = self.frame;
+    return CGRectGetMinY(frame) + CGRectGetHeight(frame);
+}
+
+- (void)setJk_bottom:(CGFloat)bottom {
+    CGRect frame = self.frame;
+    frame.origin.y = bottom - self.frame.size.height;
+    self.frame = frame;
+}
+
+- (CGFloat)jk_left {
+    return CGRectGetMinX(self.frame);
+}
+
+- (void)setJk_left:(CGFloat)x {
+    CGRect frame = self.frame;
+    frame.origin.x = x;
+    self.frame = frame;
+}
+
+- (CGFloat)jk_right {
+    return CGRectGetMinX(self.frame) + CGRectGetWidth(self.frame);
+}
+
+- (void)setJk_right:(CGFloat)right {
+    CGRect frame = self.frame;
+    frame.origin.x = right - CGRectGetWidth(self.frame);
+    self.frame = frame;
+}
+
+#pragma mark - Shortcuts for the size
+
+- (CGFloat)jk_width {
+    return CGRectGetWidth(self.frame);
+}
+
+- (void)setJk_width:(CGFloat)width {
+    CGRect frame = self.frame;
+    frame.size.width = width;
+    self.frame = frame;
+}
+
+- (CGFloat)jk_height {
+    return CGRectGetHeight(self.frame);
+}
+
+- (void)setJk_height:(CGFloat)height {
+    CGRect frame = self.frame;
+    frame.size.height = height;
+    self.frame = frame;
 }
 
 @end
