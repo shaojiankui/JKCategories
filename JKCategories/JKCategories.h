9fc2c12fa5088b7f67738ef9c4b035460f4094d4
Qilin Hu <andywhm@163.com>
2021-03-05 10:52:39 +0800

    :bug: 移除 CoreData 引入模块。

--- JKCategories/JKCategories.h
+++ JKCategories/JKCategories.h
@@ -15,14 +15,12 @@ FOUNDATION_EXPORT const unsigned char JKCategoriesVersionString[];
 // In this header, you should import all the public headers of your framework using statements like #import <JKCategories/PublicHeader.h>
 #import <JKCategories/JKUIKit.h>
 #import <JKCategories/JKQuartzCore.h>
-#import <JKCategories/JKCoreData.h>
 #import <JKCategories/JKFoundation.h>
 #import <JKCategories/JKMapKit.h>
 #import <JKCategories/JKCoreLocation.h>
 #else
 #import "JKUIKit.h"
 #import "JKQuartzCore.h"
-#import "JKCoreData.h"
 #import "JKFoundation.h"
 #import "JKMapKit.h"
 #import "JKCoreLocation.h"
